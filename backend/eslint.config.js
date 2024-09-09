e934f5387ba05981659b69119d0fb87cd795dc1f
Juan Correa <juan.correa.herrera@gmail.com>
2024-09-09 17:58:17 -0600

    chore: add and config Prettier for backend

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,12 +1,13 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
   { ignores: ["dist/"] },
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
     languageOptions: { globals: globals.node },
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier],
   },
 );
