67f503d338c1d3ee916e691f85c0c89ae6f5265f
Juan Correa <juan.correa.herrera@gmail.com>
2024-09-09 21:10:43 -0600

    feat: add loading status in the UI form

--- frontend/src/features/credit-card-validation/components/credit-card-validation-form.tsx
+++ frontend/src/features/credit-card-validation/components/credit-card-validation-form.tsx
@@ -10,28 +10,41 @@ interface CreditCardFormElement extends HTMLFormElement {
   readonly elements: FormElements;
 }
 
+type FetchStatus = "idle" | "loading" | "success" | "error";
+
 export const CreditCardValidationForm = () => {
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const [fetchStatus, setFetchStatus] = useState<FetchStatus | null>(null);
 
   const handleSubmit = async (event: React.FormEvent<CreditCardFormElement>) => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    const creditCardInputValue = form.elements.creditCardInput.value;
+    try {
+      event.preventDefault();
+      setErrorMessage(null);
+      setFetchStatus("loading");
 
-    if (creditCardInputValue.trim() === "") {
-      setErrorMessage("Please enter a credit card number");
-      return;
+      const form = event.currentTarget;
+      const creditCardInputValue = form.elements.creditCardInput.value;
+
+      if (creditCardInputValue.trim() === "") {
+        setErrorMessage("Please enter a credit card number");
+        return;
+      }
+
+      const data = await fetchCreditCardValidation(creditCardInputValue);
+
+      if (!data.isValid) {
+        setErrorMessage(data.errorMessage);
+        return;
+      }
+
+      setSuccessMessage("Credit card number is valid");
+    } catch (error) {
+      console.error(error);
+      setFetchStatus("error");
+    } finally {
+      setFetchStatus("success");
     }
-
-    const data = await fetchCreditCardValidation(creditCardInputValue);
-
-    if (!data.isValid) {
-      setErrorMessage(data.errorMessage);
-      return;
-    }
-
-    setSuccessMessage("Credit card number is valid");
   };
 
   return (
@@ -60,13 +73,17 @@ export const CreditCardValidationForm = () => {
       </div>
       <button
         type="submit"
-        className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+        disabled={fetchStatus === "loading"}
+        className={cn(
+          "w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150",
+          fetchStatus === "loading" && "cursor-not-allowed opacity-50",
+        )}
       >
         Submit
       </button>
       {successMessage && (
         <p className="mt-2 text-sm text-green-600" id="email-error">
-          {successMessage}
+          {successMessage} 👯‍♂️
         </p>
       )}
     </form>
