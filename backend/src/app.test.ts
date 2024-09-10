322d35a6ba54b38c8ee4b59c8ee044c149450292
Juan Correa <juan.correa.herrera@gmail.com>
2024-09-09 19:14:40 -0600

    feat: add request body validation to the endpoint

--- backend/src/app.test.ts
+++ backend/src/app.test.ts
@@ -7,8 +7,16 @@ const url = `${baseUrl}/${creditCardValidationUrl}`;
 
 describe(`POST ${url}`, () => {
   it("should return 200 and a valid credit card message", async () => {
-    const response = await request(app).post(url);
+    const response = await request(app).post(url).send({
+      creditCardNumber: "4556737586899855",
+    });
 
     expect(response.status).toBe(200);
+  });
+
+  it("should return status 400 when creditCardNumber is not provided", async () => {
+    const response = await request(app).post(url);
+
+    expect(response.status).toBe(400);
   });
 });
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,8 +4,21 @@ import { baseUrl, creditCardValidationUrl } from "./url-config";
 
 const app = express();
 
+app.use(express.json());
+
 app.post(`${baseUrl}/${creditCardValidationUrl}`, (request: Request, response: Response) => {
-  response.status(200).send("Hello World");
+  try {
+    const { creditCardNumber } = request.body;
+
+    if (!creditCardNumber) {
+      return response.status(400).send("creditCardNumber is required");
+    }
+
+    response.status(200).send("Hello World");
+  } catch (error) {
+    console.log(error);
+    response.status(500).send("Internal Server Error");
+  }
 });
 
 export { app };
